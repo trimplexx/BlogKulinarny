f3e8d319c0e41472c440e4ab665871f83912c9ec
Desi451 <mdesortes00@gmail.com>
2023-05-11 23:41:12 +0200

    Add project files.

---
+++ BlogKulinarny/Models/user.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BlogKulinarny.Models
+{
+    public class user
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Login")]
+        public string Login { get; set; }
+
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Mail")]
+        public string Mail { get; set; }
+
+        [Display(Name = "ActivationStatus")]
+        public bool IsAccepted { get; set; }
+
+        [Display(Name = "Role")]
+        public int RoleId { get; set; }
+
+        //Relationships
+    }
+}
