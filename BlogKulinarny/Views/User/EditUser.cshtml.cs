ad4304f091dbd4f8f2218ed7b0f2779cac439914
Filip Habryn <filihab847@student.polsl.pl>
2023-05-30 13:54:56 +0200

    views to show the recipe of the user and to edit the user

---
+++ BlogKulinarny/Views/User/EditUser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlogKulinarny.Views.User
+{
+    public class EditUserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
