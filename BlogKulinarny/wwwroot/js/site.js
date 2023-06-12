6ec3c38fb9cfaaea1be9a1fcd4fb275ea6366077
BubbleWaffle <hunter13cie@gmail.com>
2023-06-12 20:25:14 +0200

    Add script for steps in RecipeDetails

--- BlogKulinarny/wwwroot/js/site.js
+++ BlogKulinarny/wwwroot/js/site.js
@@ -17,3 +17,41 @@ function enableRadioButtons() {
         radioDifficulty.disabled = true;
     }
 }
+
+function updateBorders() {
+    var checkboxes = document.getElementsByClassName('checkbox-class');
+    for (var i = 0; i < checkboxes.length; i++) {
+        var checkbox = checkboxes[i];
+        var container = checkbox.closest('.border');
+
+        if (checkbox.checked) {
+            container.classList.remove('border-warning');
+            container.classList.remove('bg-light');
+            container.classList.add('border-success');
+            container.classList.add('bg-secondary');
+        } else {
+            container.classList.remove('border-success');
+            container.classList.remove('bg-secondary');
+            container.classList.add('border-warning');
+            container.classList.add('bg-light');
+        }
+
+        var childContainers = container.querySelectorAll('.border');
+        for (var j = 0; j < childContainers.length; j++) {
+            var childContainer = childContainers[j];
+            if (checkbox.checked) {
+                childContainer.classList.remove('border-warning');
+                childContainer.classList.remove('bg-light');
+                childContainer.classList.add('border-success');
+                childContainer.classList.add('bg-secondary');
+                childContainer.classList.add('text-decoration-line-through');
+            } else {
+                childContainer.classList.remove('border-success');
+                childContainer.classList.remove('bg-secondary');
+                childContainer.classList.remove('text-decoration-line-through');
+                childContainer.classList.add('border-warning');
+                childContainer.classList.add('bg-light');
+            }
+        }
+    }
+}
